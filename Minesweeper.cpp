0c13178e3197f1b1adc143bbee62eec3166d7db1
boombuster2000 <chanuka.williams@outlook.com>
2024-11-14 12:58:06 +0000

    Created basic tile structure

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -1,10 +1,33 @@
 ﻿#include <iostream>
 #include "raylib.h"
+#include <vector>
+
+
+struct Tile
+{
+	enum Contents
+	{
+		BOMB = -1,
+		EMPTY = 0,
+		ONE = -1,
+		TWO = 2,
+		THREE = 3,
+		FOUR = 4,
+		FIVE = 5,
+		SIX = 6,
+		SEVEN = 7,
+		EIGHT = 8
+	};
+
+	bool visible = false;
+	Contents tileContents = EMPTY;
+};
 
 int main()
 {
 	InitWindow(800, 600, "Minesweeper");
 	SetTargetFPS(60);
+
 
 	while (!WindowShouldClose())
 	{
